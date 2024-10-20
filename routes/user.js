65dd0b429f8d8300b3a85a6c329a810bec1e122e
Shubham-Sharad-Mane <shubhammane9322@gmail.com>
2024-10-20 15:29:47 +0530

    change to routes

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,6 @@ router.post("/user/register",register); // create the route for the register use
 router.post("/user/verify",verifyUser);//create the route for the verify user
 router.post("/user/login",loginUser); //create the route for the ligin user 
 router.get("/user/me", isAuth,myProfile);//create the route for the my profile and use   the isAuth middleware for the Authentiation 
-router.post("user/forgot",ForgotPassword);//creating the route for the forgot password
-router.post("user/reset",ResetPassword);//creating the route for set the new password
+router.post("/user/forgot",ForgotPassword);//creating the route for the forgot password
+router.post("/user/reset",ResetPassword);//creating the route for set the new password
 module.exports=router; 
